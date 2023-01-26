4f73a769b86e107dae9f406390bee9df47ab9fff
Hassan <aamerh@student.unimelb.edu.au>
2023-01-27 00:59:08 +1100

    Standardized the Code using Standard

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,42 @@
-const form = document.querySelector('form');
+const form = document.querySelector('form')
 
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    showSpinner();
+  e.preventDefault()
+  showSpinner()
 
-    const data = new FormData(form);
+  const data = new FormData(form)
 
-    const response = await fetch('http://localhost:8080', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            prompt: data.get('prompt'),
-        }),
-    });
+  const response = await fetch('http://localhost:8080', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
 
-    if (response.ok) {
-        const { image } = await response.json();
+  if (response.ok) {
+    const { image } = await response.json()
 
-        const result = document.querySelector('#result');
-        result.innerHTML = `<img src="${image}" width="512" />`;
-    }
-    else {
-        const err = await response.text();
-        alert(err);
-        console.error(err);
-    }
-    hideSpinner();
-});
+    const result = document.querySelector('#result')
+    result.innerHTML = `<img src="${image}" width="512" />`
+  } else {
+    const err = await response.text()
+    alert(err)
+    console.error(err)
+  }
+  hideSpinner()
+})
 
 function showSpinner() {
-    const button = document.querySelector('button');
-    button.disabled = true;
-    button.innerHTML = `Dreaming... <span class="spinner">🧠</span>`;
+  const button = document.querySelector('button')
+  button.disabled = true
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>'
 }
 
 function hideSpinner() {
-    const button = document.querySelector('button');
-    button.disabled = false;
-    button.innerHTML = `Dream`;
+  const button = document.querySelector('button')
+  button.disabled = false
+  button.innerHTML = 'Dream'
 }
--- server.js
+++ server.js
@@ -1,52 +1,50 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as dotenv from 'dotenv'
 
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-import { Configuration, OpenAIApi } from 'openai';
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI,
-});
-
-const openaiApi = new OpenAIApi(configuration);
-
-// Handle post requests to the host name
-app.post('/', async (req, res) => {
-
-    try {
-        const prompt = req.body.prompt;
-        const openaiResponse = await openaiApi.createImage({
-            prompt,
-            n: 1,
-            size: '1024x1024',
-        })
-
-        const image = openaiResponse.data.data[0].url;
-        res.send({ image });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error?.response.data.error.message);
-    }
-})
+import { Configuration, OpenAIApi } from 'openai'
 
 // Handle get requests to the host name
 
-import { readFile } from 'fs/promises';
-app.use(express.static('public'));
+import { readFile } from 'fs/promises'
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI
+})
+
+const openaiApi = new OpenAIApi(configuration)
+
+// Handle post requests to the host name
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt
+    const openaiResponse = await openaiApi.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024'
+    })
+
+    const image = openaiResponse.data.data[0].url
+    res.send({ image })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error?.response.data.error.message)
+  }
+})
+app.use(express.static('public'))
 
 app.get('/', async (req, res) => {
-    res.send(await readFile('index.html', 'utf-8'));
+  res.send(await readFile('index.html', 'utf-8'))
 })
 
 // Listen for post requests
 app.listen(8080, () => console.log('Server is up and running on Port 8080'))
 
 // Listen for get requests
-app.listen(3000, () => console.log('Website is available!'));
+app.listen(3000, () => console.log('Website is available at http://localhost:3000'))
